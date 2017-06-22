6c3233e793613ff9074cf68838bdc83ab455b653
Roxana Anabel Lafuente <roxannitha@gmail.com>
2017-06-22 00:07:41 -0300

    Clase 1: Scatter plot para analisis descriptivo

---
+++ clase1.py
@@ -0,0 +1,34 @@
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# Cargar conjunto de datos.
+dataset = load_iris()
+
+
+# Transformar a pandas dataframe para ver un resumen de los datos.
+data = pd.DataFrame(dataset.data, columns=dataset.feature_names)
+data['target'] = dataset['target']
+
+# Analisis descriptivo.
+print data.describe()
+
+
+# Scatter plot.
+X = dataset.data[:, :2]  # we only take the first two features.
+Y = dataset.target
+
+# Crear figura.
+title = "Sepal Scatter Plot"
+plt.figure(title, figsize=(8, 6))
+
+# Crear scatter plot.
+plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.Paired)
+
+# Nombrar los ejes para entender mejor la figura.
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+
+# Mostrar figura.
+plt.show()
